ddb8bdb082067861d49fcd8c34a92d4fd585c4e2
jramon901225 <jramon901225@gmail.com>
2021-11-19 22:59:57 -0500

    ok

---
+++ introhttp.js/introhttp.js/colors/colors.js
@@ -0,0 +1,13 @@
+const colors = require('colors/safe');
+
+// console.log('Buen dia'.bgRed);
+// console.log('Ya me salio el ejercicio'.cyan);
+// console.log('lorem ipsu'.magenta);
+
+
+// colors.enable()
+
+
+// Ejemplo OScar
+
+console.log((colors.rainbow('hola a todos')));
